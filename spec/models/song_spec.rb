95c3479d6334f798d75027c832955dfda55df331
Tom Dyer <tdyer1@gmail.com>
2015-02-01 19:14:30 -0500

    Added validation for song MUST be part of album

--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -1,5 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe Song, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:album) { Album.create(title: 'album1', genre: 'rock') }
+  subject { album.songs.new(title: 'song1', duration: 199, price: 2.99) }
+
+  it "should create a valid song" do
+    expect(subject.title).to eq 'song1'
+    expect(subject.duration).to eq 199
+    expect(subject.price).to be_within(0.001).of(2.99)
+  end
+
+  it "should not allow a song without a title" do
+    subject.title = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "should not allow a song without a duration less than 30 seconds" do
+    subject.duration = 29
+    expect(subject).to_not be_valid
+  end
+
+  it "should not allow a song without a duration greater than 1200" do
+    subject.duration = 1200
+    expect(subject).to_not be_valid
+  end
+
+  it "should allow a song without a duration greater than 30 and less than 1200 seconds" do
+    expect(subject).to be_valid
+    subject.duration = 1199
+    expect(subject).to be_valid
+  end
+
+  it "should not allow a valid subject that does not belong to an album" do
+    song = Song.new(title: 'song1', duration: 199, price: 2.99)
+    expect{song.save!}.to raise_error(ActiveRecord::RecordInvalid, /Album can't be blank/)
+  end
+
 end

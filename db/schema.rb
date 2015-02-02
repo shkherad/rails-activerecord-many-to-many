0c6ce6451be634cb5d978a515ed8bb0adbee3ddc
Tom Dyer <tdyer1@gmail.com>
2015-02-02 09:49:52 -0500

    Added Artist scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150130000014) do
+ActiveRecord::Schema.define(version: 20150202144802) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,14 @@ ActiveRecord::Schema.define(version: 20150130000014) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "artists", force: :cascade do |t|
+    t.string   "name"
+    t.boolean  "union_member", default: true
+    t.date     "dob"
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
   end
 
   create_table "songs", force: :cascade do |t|

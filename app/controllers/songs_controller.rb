21544be6eb1a8197079f8d0b76dd3d18ba745087
Tom Dyer <tdyer1@gmail.com>
2015-02-01 21:36:13 -0500

    Oops, forgot the song controller

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,60 +1,59 @@
 class SongsController < ApplicationController
 
-  # set the album instance for all actions
-  before_action :set_album
+  # set the parent instance and parent resource name
+  # for all actions
+  before_action :set_parent
 
-  # set the song album instance for some actions
+  # set the song parent instance for some actions
   before_action :set_song, only: [:show,:edit,:update, :destroy]
 
-  # GET /albums/:album_id/
   def index
-    @songs = @album.songs
+    @songs = @parent.songs
   end
 
-  # GET /albums/:album_id/songs/:id
   def show
   end
 
-  # GET /albums/:album_id/songs/new
   def new
-    @song = @album.songs.new
+    @song = @parent.songs.new
   end
 
-  # POST /albums/:album_id/songs/:id/edit
   def create
-    @song = @album.songs.new(song_params)
+    @song = @parent.songs.new(song_params)
 
     if @song.save
-      redirect_to album_songs_path(@album), notice: "You've created a new Song"
+      redirect_to send("#{@parent_resource}_songs_path",@parent), notice: "You've created a new Song for an #{@parent_resource}"
     else
       render :new
     end
   end
 
-  # GET /albums/:album_id/edit/songs/:id/edit
   def edit
   end
 
-  # PATCH/PUT /albums/:album_id/songs/:id
   def update
-    @song = @album.songs.find(params[:id])
+    @song = @parent.songs.find(params[:id])
 
     if @song.update(song_params)
-      redirect_to album_songs_path(@album), notice: 'Song was succesfully updated.'
+      redirect_to send("#{@parent_resource}_songs_path",@parent), notice: 'Song was succesfully updated.'
     else
       render :new
     end
   end
 
   private
-  def set_album
-    logger.debug "invoking set_album"
-    @album = Album.find(params[:album_id])
+  def set_parent
+    logger.debug "invoking set_song"
+    parent_resources = %w{ album artist}
+    if @parent_resource = parent_resources.detect { |pk| params["#{pk}_id"].present? }
+      logger.debug "parent resource is #{@parent_resource}"
+      @parent = @parent_resource.camelize.constantize.find(params[:"#{@parent_resource}_id"])
+    end
   end
 
   def set_song
     logger.debug "invoking set_song"
-    @song = @album.songs.find(params[:id])
+    @song = @parent.songs.find(params[:id])
   end
 
   def song_params

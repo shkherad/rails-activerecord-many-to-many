5f6bf32d5a3cf385dae28e07b71f9a547ab5ede8
Antony Donovan <antony.donovan@generalassemb.ly>
2016-02-22 12:10:14 -0500

    Update for `rails-activerecord-*` changes.
    missed in previous commit.

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,2 @@
 class Person < ActiveRecord::Base
-  has_many :pets, inverse_of: :person
-  belongs_to :place, inverse_of: :people
 end
--- app/models/place.rb
+++
@@ -1,3 +0,0 @@
-class Place < ActiveRecord::Base
-  has_many :people, inverse_of: :place
-end
